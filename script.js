d6496fdefe8f89fb860f70d6af5de7a977048d17
Rubén Zúñiga <ruben.zunniga.carrillo@gmail.com>
2026-09-16 17:21:13 +0200

    movimiento de etiquetas con clase 'main-text'

--- script.js
+++ script.js
@@ -1,66 +1,83 @@
-function example1() {
-    let element = document.getElementsByClassName("main-text");
+window.addEventListener("load", function(){
+    function example1() {
+        let elements = document.getElementsByClassName("main-text");
+        let totalLeft = 0;
+        const MOVE_VALUE = 1;
+        let increase = true; 
 
-    let totalLeft = 0;
-    const I_VALUE = 1;
-    let increase = true; 
+        let w = window.innerWidth - 100;
+        marginControler.max = w;
+        elements[0].style.color = "#007a0a";
+        
+        setInterval(() => {
+            // Set increase or decrease margin value
+            if (totalLeft >= w) increase = false;
+            else if (totalLeft <= 0) increase = true;
+            // Apply increase or decrease margin value
+            if (increase) totalLeft += MOVE_VALUE;
+            else totalLeft -= MOVE_VALUE;
+            // Implement margin value to HTML
+            for (let i = 0; i < elements.length; i ++) {
+                if (i === 1)
+                    elements[1].innerHTML = "Margin de animación " + totalLeft + " px";
+                else 
+                    elements[i].style.marginLeft = totalLeft + "px";
+            }
+        }, 5);
+        /*
+            // Only first element moves
+            setInterval(() => {
+                elements[0].style.marginLeft = totalLeft + "px";
+                elements[1].innerHTML = "Margin de arriba " + totalLeft + " px";
+                if (totalLeft >= w) increase = false;
+                else if (totalLeft <= 0) increase = true;
 
-    element[0].style.color = "#007a0a";
-    console.log(marginControler.max = window.innerWidth);
+                if (increase) totalLeft += MOVE_VALUE;
+                else totalLeft -= MOVE_VALUE;
 
-    setInterval(() => {
-        let w = window.innerWidth
-        // console.log(element[0].offsetWidth)
-        element[0].style.marginLeft = totalLeft + "px";
-        element[1].innerHTML = "Margin de arriba " + totalLeft + " px";
-        if (totalLeft >= w) increase = false;
-        else if (totalLeft <= 0) increase = true;
+                // console.log(elements[3].offsetWidth);
+                marginControler.value = totalLeft;
+            }, 0);
+        */
+        // setTimeout(()=>{
+        //     let totalLeft = 2;
+        //     console.log("Timeoute1 y su valor de totalLeft:" + totalLeft);
+        // }, 2000);
 
-        if (increase) totalLeft += I_VALUE;
-        else totalLeft -= I_VALUE;
+        // setTimeout(()=>{
+        //     console.log("Timeoute2 y su valor de totalLeft:" + totalLeft);
+        // }, 2000);
+    }
 
-        // console.log(element[3].offsetWidth);
-        marginControler.value = totalLeft;
-    }, 5);
+    example1();
+    // moveWithRangeInput();
 
-    setTimeout(()=>{
-        let totalLeft = 2;
-        console.log("Timeoute1 y su valor de totalLeft:" + totalLeft);
-    }, 2000);
+    function moveWithRangeInput() {    
+        let changeMargin = false;
+        let elements = document.getElementsByClassName("main-text");
+        let valueUp = 0;
+        let valueDown = 0;
+        marginControler.max = window.innerWidth - 100;
 
-    setTimeout(()=>{
-        console.log("Timeoute2 y su valor de totalLeft:" + totalLeft);
-    }, 2000);
-}
+        marginControler.addEventListener("mouseup", (e) => {
+            valueUp = marginControler.value;
+            changeMargin = false;
+        })
+        marginControler.addEventListener("mousedown", (e) => {
+            valueDown = marginControler.value;
+            changeMargin = true;        
+        })
 
-// example1();
-t1();
+        // setInterval(() => {
+        //     marginControler.value = valueDown - valueUp;
+        //     elements[0].style.marginLeft = marginControler.value + "px";
+        // }, 1);
+        setInterval(() => {
+            if(changeMargin){
+                elements[0].style.marginLeft = marginControler.value + "px";
+                console.log(marginControler.value)
+            }
+        }, 1);
 
-function t1() {    
-    let changeMargin = false;
-    let element = document.getElementsByClassName("main-text");
-    let valueUp = 0;
-    let valueDown = 0;
-    marginControler.max = window.innerWidth - 100;
-
-    marginControler.addEventListener("mouseup", (e) => {
-        valueUp = marginControler.value;
-        changeMargin = false;
-    })
-    marginControler.addEventListener("mousedown", (e) => {
-        valueDown = marginControler.value;
-        changeMargin = true;        
-    })
-
-    // setInterval(() => {
-    //     marginControler.value = valueDown - valueUp;
-    //     element[0].style.marginLeft = marginControler.value + "px";
-    // }, 1);
-    setInterval(() => {
-        if(changeMargin){
-            element[0].style.marginLeft = marginControler.value + "px";
-            console.log(marginControler.value)
-        }
-    }, 1);
-
-}
+    }
+});
